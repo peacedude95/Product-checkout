35aa0d3f9af2cf58ec7222703c39575b9be07f36
peace <mulcaheygeorge@gmail.com>
2024-10-01 19:52:56 +0100

    initial project setup

---
+++ src/main/java/mulcahey/gm/productCheckout/ProductCheckoutApplication.java
@@ -0,0 +1,13 @@
+package mulcahey.gm.productCheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCheckoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mulcahey/gm/productCheckout/ProductCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package mulcahey.gm.productCheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
